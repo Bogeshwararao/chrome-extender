52eaa8908b159f6f37bb9c9e8ed76df17bf63fe8
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-01-04 11:23:51 +0530

    much about save

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ let ulel = document.getElementById("ul-el")
 let inputbtn = document.getElementById("input-btn")
 inputbtn.addEventListener("click",function(){
    myleads.push(inputel.value)
-   console.log(myleads)
+   inputel.value =  ""
+   renderleads()
 })
+function renderleads(){
+   let lisitems =""
+   for( let i = 0 ; i<myleads.length;i++){
+        lisitems +=
+         <li>
+           <a target='_blank' herf='${myleads[i]}'>
+              ${myleads[i]}
+              </a>
+                      </li>    
+   }
+   ulel.innerHTML=lisitems
+}
+
 
